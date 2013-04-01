f6bf3ffd159f09eeac9dd3c2de0a2d0c3e9d4a2c
Artanis <283186127@qq.com>
2013-04-02 00:29:32 +0800

    Finish exporting opencv-free APIs.

--- Export/Export.cpp
+++ Export/Export.cpp
@@ -3,20 +3,44 @@
 
 #include "stdafx.h"
 #include "Export.h"
+#include "../System.Image/System.Image.h"
+#include <cv.h>
+#include <vector>
+#include <map>
+using namespace System::Image;
+using namespace cv;
+using namespace std;
 
-
-// This is an example of an exported variable
-EXPORT_API int nExport=0;
-
-// This is an example of an exported function.
-EXPORT_API int fnExport(void)
+EXPORT_API vector<pair<vector<Position>, Matrix<float>>> PerformHitmap(
+    const Matrix<uchar>& image, bool thinning)
 {
-	return 42;
-}
+    Hitmap hitmap;
 
-// This is the constructor of a class that has been exported.
-// see Export.h for the class definition
-CExport::CExport()
-{
-	return;
+    Mat cvImage(image.rows, image.cols, CV_8U);
+    for (int i = 0; i < image.rows; i++)
+        for (int j = 0; j < image.cols; j++)
+            cvImage.at<uchar>(i, j) = image.at(i, j);
+
+    Hitmap::Info tmp = hitmap.GetFeatureWithPreprocess(cvImage, thinning);
+
+    vector<pair<vector<Position>, Matrix<float>>> result;
+    for (int i = 0; i < tmp.size(); i++)
+    {
+        const Tuple<vector<Point>, Mat>& item = tmp[i];
+        const vector<Point>& item1 = item.Item1();
+        const Mat& item2 = item.Item2();
+
+        vector<Position> vec;
+        for (int j = 0; j < item1.size(); j++)
+            vec.push_back(Position(item1[j].x, item1[j].y));
+    
+        Matrix<float> mat(item2.rows, item2.cols);
+        for (int j = 0; j < item2.rows; j++)
+            for (int k = 0; k < item2.cols; k++)
+                mat.at(j, k) = item2.at<float>(j, k);
+
+        result.push_back(make_pair(vec, mat));
+    }
+
+    return result;
 }
--- Export/Export.h
+++ Export/Export.h
@@ -10,13 +10,99 @@
 #define EXPORT_API __declspec(dllimport)
 #endif
 
-// This class is exported from the Export.dll
-class EXPORT_API CExport {
-public:
-	CExport(void);
-	// TODO: add your methods here.
+#include "../System/Type.h"
+using namespace System;
+
+template<typename T>
+struct EXPORT_API Matrix
+{
+    Matrix(int rows, int cols)
+    {
+        m = new T*[rows];
+        for (int i = 0; i < rows; i++)
+            m[i] = new T[cols];
+    
+        this->rows = rows;
+        this->cols = cols;
+    }
+
+    Matrix(const Matrix& other)
+    {
+        this->rows = other.rows;
+        this->cols = other.cols;
+
+        m = new T*[rows];
+        for (int i = 0; i < rows; i++)
+        {
+            m[i] = new T[cols];
+            for (int j = 0; j < cols; j++)
+                m[i][j] = other[i][j];
+        }
+    }
+
+    ~Matrix()
+    {
+        clear();
+    }
+
+    Matrix& operator=(const Matrix& other)
+    {
+        if (m == other.m)
+            return this;
+
+        if (rows != other.rows || cols != other.cols)
+        {
+            clear();
+
+            rows = other.rows;
+            cols = other.cols;
+
+            m = new T*[rows];
+            for (int i = 0; i < rows; i++)
+                m[i] = new T[cols];
+        }
+        
+        for (int i = 0; i < rows; i++)
+            for (int j = 0; j < cols; j++)
+                m[i][j] = other.m[i][j];
+
+        return this;
+    }
+
+    T& at(int row, int col)
+    {
+        return m[row][col];
+    }
+
+    const T& at(int row, int col) const
+    {
+        return m[row][col];
+    }
+
+    void clear()
+    {
+        for (int i = 0; i < rows; i++)
+            delete[] m[i];
+        delete[] m;
+    }
+
+    T** m;
+    int rows, cols;
 };
 
-extern EXPORT_API int nExport;
+struct EXPORT_API Position
+{
+    Position(int x, int y)
+    {
+        this->x = x;
+        this->y = y;
+    }
 
-EXPORT_API int fnExport(void);
+    int x, y;
+};
+
+typedef unsigned char uchar;
+
+EXPORT_API vector<pair<vector<Position>, Matrix<float>>> PerformHitmap(
+    const Matrix<uchar>& image, bool thinning);
+
