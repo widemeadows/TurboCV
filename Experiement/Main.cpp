f20d72a42b3cbfe607e09c9f344090c17e593e6a
Artanis <283186127@qq.com>
2013-04-05 12:24:24 +0800

    Change some experimental codes.

--- Experiement/Main.cpp
+++ Experiement/Main.cpp
@@ -674,7 +674,7 @@ int main()
     //printf("\n");
 
     //Batch("sketches", false);
-    Batch("oracles", true);
+    //Batch("oracles", true);
 
     //Mat trans = getRotationMatrix2D(Point(image.rows / 2, image.cols / 2), -20, 1);
     //warpAffine(image, image, trans, image.size());
