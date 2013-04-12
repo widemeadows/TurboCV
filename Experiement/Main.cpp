a5aff66c2a8a578c7955706d3da2e848e3542856
Artanis <283186127@qq.com>
2013-04-12 10:57:08 +0800

    Change some experimental codes.

--- Experiement/Main.cpp
+++ Experiement/Main.cpp
@@ -697,11 +697,11 @@ int main()
     //GlobalFeatureCrossValidation("oracles_png", GHOG());
     //printf("\n");
 
-    EdgeMatchingCrossValidation("oracles", OCM(), true);
-    printf("\n");
-
-    //LocalFeatureTest("oracles_png", Test(), 1500);
+    //EdgeMatchingCrossValidation("oracles", OCM(), true);
     //printf("\n");
+
+    LocalFeatureTest("oracles_png", Test(), 1500);
+    printf("\n");
 
     //Batch("sketches", false);
     //Batch("oracles", true);
