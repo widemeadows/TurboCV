f2dac5f0ffd7d65261ea5ef6e229b9ece672e820
Artanis <283186127@qq.com>
2013-04-27 16:10:46 +0800

    Add the missing file in the last commit.

---
+++ UnitTest/BasicTypeTest.cpp
@@ -0,0 +1,26 @@
+#include "stdafx.h"
+#include "CppUnitTest.h"
+#include "../System/Type.h"
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+using namespace TurboCV::System;
+
+namespace UnitTest
+{        
+    TEST_CLASS(BasicTypeTest)
+    {
+    public:
+        TEST_METHOD(ParseTest)
+        {
+            Assert::AreEqual(123, Int::Parse(String("123")));
+            Assert::AreEqual(123.5F, Float::Parse(String("123.5")));
+            Assert::AreEqual(123.555555, Double::Parse(String("123.555555")));
+        }
+
+        TEST_METHOD(ToStringTest)
+        {
+            Assert::AreEqual((const char*)String("123"), Int::ToString(123));
+            Assert::AreEqual((const char*)String("123.5"), Float::ToString(123.5));
+            Assert::AreEqual((const char*)String("123.555555"), Double::ToString(123.555555));
+        }
+    };
+}
